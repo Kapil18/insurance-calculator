8ed13ebbee868e47b6e46f98595d074b8a0f1413
Neeraj Bhardwaj <neeraj.bhardwaj@avizva.com>
2021-11-15 11:20:54 +0530

    navbaradded

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Logo
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Quote
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/">
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Utility
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Support
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <button className="btn btn-outline-success mx-3" type="submit">
+                Log In
+              </button>
+              <button className="btn btn-outline-success Mx-3" type="submit">
+                SignUp
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
